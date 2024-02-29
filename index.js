031e0449fbb14d39670b2e5986bbe682ed4f8641
PrateekTechmatys <prateeky562@gmail.com>
2024-02-29 16:20:07 +0530

    Working with delete button

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ let createNote = document.querySelector('#createNote');
 let newAddPara= document.querySelector('#new');
 console.log(createNote,main,notes,newAddPara)
 
-
+let notesPara= document.querySelector('.para');
 createNote.addEventListener('click', showPara);
 
+function show(){
+    newAddPara.innerHTML= localStorage.getItem("notes");
+}
+show();
 function showPara(){
     let newPara = document.createElement('p');
     newPara.setAttribute('contenteditable', true);
@@ -19,11 +23,33 @@ function showPara(){
     newButton.src="./pr.png"
     newButton.classList.add('newbutton')
     newAddPara.appendChild(newPara).appendChild(newButton);
-    newButton.addEventListener('click', deleteF);
+    // newButton.addEventListener('click', deleteF);
 }
 
-function deleteF(){
-    let para = document.querySelector('p');
-    console.log(para);
-    newAddPara.removeChild(para);
+// function deleteF(){
+//     let para = document.querySelector('p');
+//     console.log(para);
+//     newAddPara.removeChild(para);
+//     updateStorage();
+
+
+// }
+
+newAddPara.addEventListener('click', function(e){
+    if(e.target.tagName==="IMG"){
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+    else if(e.target.tagName==="P"){
+        notesPara=document.querySelectorAll('.para');
+        notesPara.forEach(nt =>{
+            nt.onkeyup = function(){
+                updateStorage();
+            }
+        })
+    }
+})
+
+function updateStorage(){
+    localStorage.setItem('notes',newAddPara.innerHTML);
 }
